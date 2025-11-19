bafca1a22371608ffe5295c8e7cd621cdf6daccc
huymai96 <huymai96@users.noreply.github.com>
2025-11-19 11:43:26 -0600

    Add Target mode support (7 units per case, 1-1-2-2-1 size scale)

---
+++ src/config/targetConfig.js
@@ -0,0 +1,17 @@
+﻿// Target packing configuration
+export const TARGET_CONFIG = {
+  UNITS_PER_CASE: 7,
+  SIZE_SCALE: {
+    S: 1,
+    M: 1,
+    L: 2,
+    XL: 2,
+    '2X': 1,
+  },
+};
+export function getExpectedTotalForSize(size, numberOfCartons) {
+  return TARGET_CONFIG.SIZE_SCALE[size] * numberOfCartons;
+}
+export function getExpectedTotalUnits(numberOfCartons) {
+  return TARGET_CONFIG.UNITS_PER_CASE * numberOfCartons;
+}
